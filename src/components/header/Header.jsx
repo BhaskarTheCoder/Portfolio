5619dc5bad5874c2392240591d7fb5a8cc6dabda
Uday Bhaskar Valapadasu <109142269+BhaskarTheCoder@users.noreply.github.com>
2024-10-22 15:34:09 -0500

    Complete Home Component
    - Implemented Home, Social, Data & ScrollDown Sections.
    - Added my professional picture.

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -10,7 +10,7 @@ const Header = () => {
     <header className="header">
       <nav className="nav container">
         <a href="index.html" className="nav_logo">
-          Uday
+          Uday Bhaskar Valapadasu
         </a>
 
         <div className={Toggle ? "nav_menu show-menu" : "nav_menu"}>
